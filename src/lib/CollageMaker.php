76da4548366621a74041e2f753bf3242c4aac94e
Dima-macbook <dima@virtuman.com>
2018-04-16 23:10:24 +0300

    added pre-receive hook for code style checker

---
+++ src/lib/CollageMaker.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima
+ * Date: 16/04/2018
+ * Time: 23:02
+ */
+
+namespace LenPRO\Collage;
+
+class CollageMaker {
+    public function __construct() {
+    }
+}
