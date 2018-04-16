699d61c2bf3874324dcc545932b36c7a54393a8e
Dima-macbook <dima@virtuman.com>
2018-04-16 23:57:59 +0300

    added stub-class for collage maker

--- src/lib/CollageMaker.php
+++ src/lib/CollageMaker.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /*
  * LenPRO/Collage project
@@ -7,6 +8,35 @@
 namespace LenPRO\Collage;
 
 class CollageMaker {
-    public function __construct() {
+    private $config = [];
+
+    public function __construct(array $config) {
+        $this->setup($config);
+    }
+
+    private function setup(array $config): void {
+        $this->config = array_merge($this->config, $config);
+    }
+
+    public function setCollageSize(int $width, int $height): self {
+        $this->config['collage']['width'] = $width;
+        $this->config['collage']['height'] = $height;
+
+        return $this;
+    }
+
+    public function setImages(array $images): self {
+        if (4 != count($images)) {
+            throw new \Exception("unsupported count of images");
+        }
+        foreach ($images as $image) {
+        }
+        $this->config['images'] = $images;
+        return $this;
+    }
+
+    public function makeCollage() {
+        $collageConfig = $this->config['collage'];
+        $images = $this->config['images'];
     }
 }
